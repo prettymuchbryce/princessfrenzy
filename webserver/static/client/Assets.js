fd6b33da5eec4c9e616be1b44b17a84118317f5f
Bryce Neal <brycedneal@bryce-mbp.local>
2012-12-23 02:44:56 -0800

    Reorganizing, Darkening game using paper texture.

--- webserver/static/client/Assets.js
+++ webserver/static/client/Assets.js
@@ -1,25 +1,20 @@
 
 function initAssets(prefix) {
 	ASSET_ARROW = new Image();
-	ASSET_ARROW.src = prefix + "assets/arrows.png";
+	ASSET_ARROW.src = prefix + "assets/images/arrows.png";
 	
 	ASSET_PLAYER = new Image();
-	ASSET_PLAYER.src = prefix + "assets/chars.png";
+	ASSET_PLAYER.src = prefix + "assets/images/chars.png";
 	
 	ASSET_TILES = new Image();
-	ASSET_TILES.src = prefix + "assets/tiles.png"
-	
-	ASSET_PRINCESS = new Image();
-	ASSET_PRINCESS.src = prefix + "assets/princess.png"
-	
+	ASSET_TILES.src = prefix + "assets/images/tiles.png"
 	
 	ASSET_EXPLOSION = new Image();
-	ASSET_EXPLOSION.src = prefix + "assets/flames.png"
+	ASSET_EXPLOSION.src = prefix + "assets/images/flames.png"
 	
-	soundIntro = new Audio(prefix + "assets/intro.wav"); // buffers automatically when created
-	soundShoot = new Audio(prefix + "assets/shoot.wav"); // buffers automatically when created
-	soundPrincess = new Audio(prefix + "assets/princess.wav"); // buffers automatically when created
-	soundDie = new Audio(prefix + "assets/die.wav"); // buffers automatically when created
-	soundMove = new Audio(prefix + "assets/move.wav");
-	soundRound = new Audio(prefix + "assets/round.wav");
+	soundIntro = new Audio(prefix + "assets/audio/intro.wav"); // buffers automatically when created
+	soundShoot = new Audio(prefix + "assets/audio/shoot.wav"); // buffers automatically when created
+	soundDie = new Audio(prefix + "assets/audio/die.wav"); // buffers automatically when created
+	soundMove = new Audio(prefix + "assets/audio/move.wav");
+	soundRound = new Audio(prefix + "assets/raudio/ound.wav");
 }
